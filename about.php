916ac8abc0e06594b00171df0a338fb1e1a2a76d
Johnmn <48591148+Johnmn@users.noreply.github.com>
2022-09-24 00:25:31 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,405 @@
+<?php include('header.php'); ?>
+<body>
+	<style type="text/css">
+		#background {
+	
+	margin: 0;
+	padding: 0;
+}
+#page {
+	margin: 0 auto;
+	overflow: hidden;
+	padding: 0 10px;
+	width: 1200px;
+}
+.accordion {
+    margin-bottom: 20px;
+}
+#content {
+	background: #fff;
+	float: left;
+	margin: 0;
+	padding: 0;
+	width: 950px;
+}
+#content #header {
+	margin: 0;
+	padding: 0;
+	position: relative;
+	width: 938px;
+}
+#content #header h1 {
+	color: #fff;
+	top:40px;
+	font-family: Georgia, serif;
+	font-size: 75px;
+	font-style: italic;
+	font-weight: normal;
+	left: 43px;
+	margin: 0;
+	padding: 0;
+	position: absolute;
+	text-transform: uppercase;
+	z-index: 3;
+}
+#content #header img {
+	display: block;
+}
+#content #header div {
+	background: url(../images/bg-transparent.png) repeat-x left bottom;
+	bottom: 0;
+	left: 0;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	position: absolute;
+	width: 700px;
+	z-index: 3;
+}
+#content #header div i {
+	color: #fff;
+	display: block;
+	float: left;
+	font-family: Georgia, serif;
+	font-size: 40px;
+	line-height: 32px;
+	margin: 0;
+	padding: 225px 0 0 54px;
+	width: 372px;
+}
+#content #header div i .more {
+	background: url(../images/icons.png) no-repeat 0 -355px;
+	color: #eee778;
+	font-size: 24px;
+	margin: 0;
+	padding: 0 0 0 35px;
+	text-decoration: none;
+	text-transform: capitalize;
+}
+#content #header div i .more:hover {
+	text-decoration: underline;
+}
+#content #header div img {
+	display: block;
+	float: right;
+}
+#content #body {
+	margin: 0;
+	overflow: hidden;
+	padding: 0 0 24px;
+}
+#content #body .header {
+	margin: 0;
+	overflow: hidden;
+	padding: 0;
+}
+#content #body .header div {
+	float: left;
+	margin: 0;
+	padding: 0;
+	position: relative;
+	width: 350px;
+}
+#content #body .header div img {
+	display: block;
+}
+#content #body .header div div, #content #body .header div i {
+	display: block;
+	margin: 0;
+	padding: 0;
+	position: absolute;
+	z-index: 3;
+}
+#content #body .header div div {
+	background: url(../images/bg-transparent2.png) repeat;
+	border-radius: 10px;
+	bottom: 20px;
+	left: 0;
+	padding: 20px;
+	width: 195px;
+}
+#content #body .header div div p, #content #body .header div div b {
+	color: #fff;
+	font-family: Georgia, serif;
+	font-size: 17px;
+	font-style: italic;
+	line-height: 18px;
+	margin: 0;
+	padding: 0;
+}
+#content #body .header div div p {
+	padding: 15px 0;
+}
+#content #body .header div div b {
+	font-size: 26px;
+	font-weight: normal;
+}
+#content #body .header div div a {
+	background: url(../images/icons.png) no-repeat 0 -383px;
+	color: #b84e64;
+	font-family: Georgia, serif;
+	font-size: 18px;
+	font-style: italic;
+	line-height: 18px;
+	margin: 0;
+	padding: 0 0 0 27px;
+	text-decoration: none;
+	text-transform: capitalize;
+}
+#content #body .header div div a:hover {
+	text-decoration: underline;
+}
+#content #body .header div i {
+	background: url(../images/bg-transparent3.png) repeat;
+	border-radius: 10px;
+	bottom: 8px;
+	color: #fff;
+	font-family: Georgia, serif;
+	font-size: 32px;
+	left: 22px;
+	margin: 0;
+	padding: 5px 15px;
+	width: 276px;
+}
+#content #body .header div i a {
+	background: url(../images/icons.png) no-repeat 0 -400px;
+	color: #eee778;
+	font-size: 18px;
+	margin: 0;
+	padding: 0 0 0 27px;
+	text-decoration: none;
+	text-transform: capitalize;
+}
+#content #body .header div i a:hover {
+	text-decoration: underline;
+}
+#content #body .body {
+	margin: 0;
+	padding: 0;
+}
+#content #body p {
+	color: #7c7c7c;
+	font-size: 13px;
+	line-height: 18px;
+	margin: 0;
+	padding: 0;
+}
+#content #body p a {
+	color: #7c7c7c;
+	
+}
+#content #body p a:hover {
+	color: #b84e64;
+}
+#content #body .body ul {
+	margin: 0;
+	overflow: hidden;
+	padding: 27px 0;
+}
+#content #body .body ul li {
+	float: left;
+	list-style: none;
+	margin: 0 0 0 40px;
+	padding: 0;
+	text-align: center;
+	width: 180px;
+}
+#content #body .body ul li a.figure {
+	display: block;
+	margin: 0;
+	padding: 0;
+}
+#content #body .body ul li a.figure:hover {
+	opacity: 0.8;
+}
+#content #body .body ul li a.figure img {
+	border: 0;
+	display: block;
+}
+#content #body .body ul li h3 {
+	color: #b84e64;
+	font-family: Georgia, serif;
+	font-size: 22px;
+	font-weight: normal;
+	margin: 0;
+
+	text-align: center;
+	text-transform: capitalize;
+}
+#content #body .body ul li p {
+	text-align: center;
+}
+#content #body .body ul li a.more {
+	color: #b84e64;
+	font-size: 10px;
+	line-height: 18px;
+	text-decoration: none;
+	text-transform: uppercase;
+}
+#content #body .body ul li a.more:hover {
+	text-decoration: underline;
+}
+
+	</style>
+    <div id="background">
+	
+        <div id="page">
+            <div id="content">
+                <div class="hero-unit-table">
+                    <div id="header">
+                        
+
+                    </div>
+                    <div id="body">
+
+
+
+
+
+
+
+
+                        <h3>About US</h3>
+                        <div class="hero-unit-table">
+
+							<div id="accordion">
+										<h3>About The Company</h3>
+										<div> Football Ticket  gives you a 100% guarantee on its staff's quality and service: We have made a lot of effort building a skilled & professional Customer care team just for you. We guarantee the best professional service in the market. </div>
+										<h3>Mission</h3>
+										<div>To build football into a viable business for all involved (owners, players, media and officials.) 
+										</div>
+										<h3>Vision</h3>
+										<div>To have the most reliable and secure ticket selling system </div>
+										<h3>About the Developer</h3>
+										<div>J and M (John and Mutunga ) Software Solutions is a software firm which created by a student provides software to cater every business needs. As my tag line
+										says" Your Problem ,Our Solution".</div>
+							</div>
+                        </div>
+                    </div>
+                    <div id="footer">
+                        <?php include('footer.php'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+		<script src="external/jquery/jquery.js"></script>
+<script src="jquery-ui.js"></script>
+<script>
+
+$( "#accordion" ).accordion();
+
+
+
+var availableTags = [
+	"ActionScript",
+	"AppleScript",
+	"Asp",
+	"BASIC",
+	"C",
+	"C++",
+	"Clojure",
+	"COBOL",
+	"ColdFusion",
+	"Erlang",
+	"Fortran",
+	"Groovy",
+	"Haskell",
+	"Java",
+	"JavaScript",
+	"Lisp",
+	"Perl",
+	"PHP",
+	"Python",
+	"Ruby",
+	"Scala",
+	"Scheme"
+];
+$( "#autocomplete" ).autocomplete({
+	source: availableTags
+});
+
+
+
+$( "#button" ).button();
+$( "#radioset" ).buttonset();
+
+
+
+$( "#tabs" ).tabs();
+
+
+
+$( "#dialog" ).dialog({
+	autoOpen: false,
+	width: 400,
+	buttons: [
+		{
+			text: "Ok",
+			click: function() {
+				$( this ).dialog( "close" );
+			}
+		},
+		{
+			text: "Cancel",
+			click: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+	]
+});
+
+// Link to open the dialog
+$( "#dialog-link" ).click(function( event ) {
+	$( "#dialog" ).dialog( "open" );
+	event.preventDefault();
+});
+
+
+
+$( "#datepicker" ).datepicker({
+	inline: true
+});
+
+
+
+$( "#slider" ).slider({
+	range: true,
+	values: [ 17, 67 ]
+});
+
+
+
+$( "#progressbar" ).progressbar({
+	value: 20
+});
+
+
+
+$( "#spinner" ).spinner();
+
+
+
+$( "#menu" ).menu();
+
+
+
+$( "#tooltip" ).tooltip();
+
+
+
+$( "#selectmenu" ).selectmenu();
+
+
+// Hover states on the static widgets
+$( "#dialog-link, #icons li" ).hover(
+	function() {
+		$( this ).addClass( "ui-state-hover" );
+	},
+	function() {
+		$( this ).removeClass( "ui-state-hover" );
+	}
+);
+</script>
+</body>
+</html>
